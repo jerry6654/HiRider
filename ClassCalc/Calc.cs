e8d565c726bdf00b2a5f6a1b06a732a4ad267f91
xlive <2115931225@qq.com>
2023-09-27 04:54:49 +0800

    hello

---
+++ ClassCalc/Calc.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassCalc;
+
+public static class Calc
+ {
+     public static double Mul(double x, double y)
+     {
+         return x * y;
+     }
+ }
---
+++ Main/Program.cs
@@ -0,0 +1,42 @@
+﻿
+/*
+using ClassCalc;
+
+var result = Calc.Mul(152, 26);
+Console.WriteLine(result);
+*/
+
+string firstFriend = "Jerry";
+string secondFriend = "西门吹雪";
+var hello = "         hello            ";
+var hello1 = "-------hello---------";
+
+Console.WriteLine($"我有两个朋友，他们是{firstFriend}，还有{secondFriend}");
+Console.WriteLine($"{secondFriend}含有{secondFriend.Length}个字符");
+Console.WriteLine($"{firstFriend}含有{firstFriend.Length}个字符");
+
+Console.WriteLine($"{hello}");
+Console.WriteLine($"{hello1}");
+
+
+Console.WriteLine($"{hello.TrimStart()}");
+Console.WriteLine($"{hello.TrimEnd()}");
+Console.WriteLine($"{hello.Trim()}");
+
+hello1 = hello1.Replace("hello", "你好");
+Console.WriteLine(hello1);
+
+Console.WriteLine(hello.ToUpper().TrimStart());
+Console.WriteLine(hello.TrimStart().ToUpper());
+
+
+string s = "you say goodbye, and i say hello";
+Console.WriteLine(s);
+Console.WriteLine(s.Contains("goodbey"));
+Console.WriteLine(s.Contains("goodbye"));
+
+
+
+
+
+
