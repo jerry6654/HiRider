41cc4a82cd05307d12175495c036cf60a4eac09b
xlive <2115931225@qq.com>
2023-09-28 05:40:28 +0800

    aaa

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,6 @@
 ﻿// -------------------1.委托---------------------------
+
+// 内置委托
 Calc calc = new();
 
 Action action = new Action(calc.Show);
@@ -13,6 +15,11 @@ Console.WriteLine(func.Invoke(x, y));
 Console.WriteLine(func(x, y));
 Console.WriteLine(calc.Mul(x, y));
 
+// 自定义委托
+
+Type t = typeof(Action);
+Console.WriteLine(t.IsClass);
+
 class Calc
 {
     public void Show()
@@ -20,8 +27,18 @@ class Calc
         Console.WriteLine("委托练习");
     }
 
-    public int Mul(int x, int y)
+    public int ADD(int x, int y)
     {
+        return x + y;
+    }
+        public int Mul(int x, int y) {
         return x * y;
     }
+    
 }
+
+
+
+
+
+
